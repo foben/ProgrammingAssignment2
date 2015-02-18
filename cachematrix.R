531d95f4b7909220f812000d25414e2c17d7bcd2
Felix Obenauer <f.obenauer@gmail.com>
2015-02-18 23:20:00 +0100

    Add comments to functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,15 @@
-## Put comments here that give an overall description of what your
-## functions do
+## "makeCacheMatrix" takes a matrix as parameter and 
+## creates an object of a matrix whose inverse Matrix can be cached.
+## The "cacheSolve" function takes such an object and returns the inverse matrix,
+## either by retrieving a cached value or calculating it.
 
-## Write a short comment describing this function
-
+## Create a new cachable matrix from a standard matrix
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
-
-## Write a short comment describing this function
-
+## Calculate the inverse of the supplied matrix,
+## returning a cached value if possible
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
