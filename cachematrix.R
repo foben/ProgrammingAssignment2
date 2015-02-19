66644a7bd77b63e32557b7de591e203ee985abce
Felix Obenauer <f.obenauer@gmail.com>
2015-02-19 21:18:55 +0100

    Add more indentation, line break

--- cachematrix.R
+++ cachematrix.R
@@ -5,29 +5,32 @@
 
 ## Create a new cachable matrix from a standard matrix
 makeCacheMatrix <- function(x = matrix()) {
-  inv_matrix <- NULL
-  set <- function(y) {
-    x <<- y
-    ## If the internal matrix is changed via set,
-    ## the cached value is probably incorrect.
-    inv_matrix <<- NULL
-  }
-  get <- function() x
-  setInverse <- function(inv) inv_matrix <<- inv
-  getInverse <- function() inv_matrix
-  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+    inv_matrix <- NULL
+    set <- function(y) {
+        x <<- y
+        ## If the internal matrix is changed via set,
+        ## the cached value is probably incorrect.
+        inv_matrix <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inv) inv_matrix <<- inv
+    getInverse <- function() inv_matrix
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 ## Calculate the inverse of the supplied matrix,
 ## returning a cached value if possible
 cacheSolve <- function(x, ...) {
-  inv <- x$getInverse()
-  if ( !is.null(inv) ) {
-    message("Returning cached data")
-  } else {
-    inv <- solve(x$get())
-    x$setInverse(inv)
-  }
-  ## At this point, "inv" contains the correct cached matrix.
-  inv
+    inv <- x$getInverse()
+    if ( !is.null(inv) ) {
+        message("Returning cached data")
+    }
+    else {
+        inv <- solve(x$get())
+        x$setInverse(inv)
+    }
+    ## At this point, "inv" contains the correct cached matrix.
+    inv
 }
